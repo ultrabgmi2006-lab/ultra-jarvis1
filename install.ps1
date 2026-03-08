660b7914135117f2e6007efd16d4e5132f0b5be7
ultrabgmi2006-lab <ultrabgmi2006@gmail.com>
2026-03-08 18:43:18 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,11 @@
+Write-Host "Installing Ultra Jarvis..."
+
+git clone https://github.com/YOUR_USERNAME/ultra-jarvis.git
+
+cd ultra-jarvis
+
+py -3.11 -m pip install -r requirements.txt
+
+Write-Host "Starting Jarvis..."
+
+py -3.11 main.py
