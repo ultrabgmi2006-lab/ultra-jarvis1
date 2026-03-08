04d4f8336143c3a8282912c14edc5551e95b8261
ultrabgmi2006-lab <ultrabgmi2006@gmail.com>
2026-03-08 18:43:40 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Installing Ultra Jarvis..."
+
+git clone https://github.com/YOUR_USERNAME/ultra-jarvis.git
+
+cd ultra-jarvis
+
+pip install -r requirements.txt
+
+python main.py
